4d5ec77ab3c36f6c3043846706e32e2ee5cb3b9a
Vishvesh Dave <vishvesh01@yahoo.com>
2017-11-15 22:03:24 -0500

    final update to twitter key's

--- keys.js
+++ keys.js
@@ -4,8 +4,8 @@ var twitterKeys = {
   consumer_key: '35mEKJSjtcBNHWaipVN4o7fLx',
   consumer_secret: 'g80oqVnJqiaAjKGuL1Ty9FL80dT7JqTgJxvJr8F5o0bbJZnRku',
   access_token_key: '213255676-oNcZkNvq7BoHfKmmNrLbrV8bj6XbH5n0obA9SoDj',
-  access_token_secret: 'kThDeAeS6DZAFHRdUFAKwJm9ySXGobSDO5bUS9G7W51KW',
-}
+  access_token_secret: 'kThDeAeS6DZAFHRdUFAKwJm9ySXGobSDO5bUS9G7W51KW'
+};
 
 var spotifyKeys = {
     client_id: '0cc0eb1c849b44deb308649ce73e12cc',
