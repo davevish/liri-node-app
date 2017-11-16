d0041cf129f345bfdbe1a95131916dd028596c96
Vishvesh Dave <vishvesh01@yahoo.com>
2017-11-15 21:55:51 -0500

    fixed twitter keys

--- keys.js
+++ keys.js
@@ -1,10 +1,10 @@
 console.log('this is loaded');
 
 var twitterKeys = {
-  consumer_key: '<35mEKJSjtcBNHWaipVN4o7fLx>',
-  consumer_secret: '<g80oqVnJqiaAjKGuL1Ty9FL80dT7JqTgJxvJr8F5o0bbJZnRku>',
-  access_token_key: '<213255676-oNcZkNvq7BoHfKmmNrLbrV8bj6XbH5n0obA9SoDj>',
-  access_token_secret: '<kThDeAeS6DZAFHRdUFAKwJm9ySXGobSDO5bUS9G7W51KW>',
+  consumer_key: '35mEKJSjtcBNHWaipVN4o7fLx',
+  consumer_secret: 'g80oqVnJqiaAjKGuL1Ty9FL80dT7JqTgJxvJr8F5o0bbJZnRku',
+  access_token_key: '213255676-oNcZkNvq7BoHfKmmNrLbrV8bj6XbH5n0obA9SoDj',
+  access_token_secret: 'kThDeAeS6DZAFHRdUFAKwJm9ySXGobSDO5bUS9G7W51KW',
 }
 
 var spotifyKeys = {
